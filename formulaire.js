92e126d5df60392f3bd59d5b66071611a2351fe2
Julien RICHARD <codage@richard.plus>
2026-01-15 00:32:56 +0100

    Essai pour Tuto

--- formulaire.js
+++ formulaire.js
@@ -1,16 +1,17 @@
 function action_javascript() {
 
+    var informations=document.getElementById('informations');
     var email=document.getElementById('email');
     var bouton_remettre=document.getElementById('bouton_java_remettre');
 
     if (bouton_remettre.style.display!='') {
-        email.style.display='none';
+        informations.style.display='none';
         document.getElementById('label_email').style.display='none';
         document.getElementById('bouton_java_enlever').style.display='none';
         bouton_remettre.style.display='';
 
     } else {
-        email.style.display='';
+        informations.style.display='';
         email.value="et en plus on change le texte";
         document.getElementById('label_email').style.display='';
         document.getElementById('bouton_java_enlever').style.display='';
